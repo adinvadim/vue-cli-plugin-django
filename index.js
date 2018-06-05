b371c034133774e0617dd4a0b62c0667d9a63ee5
adinvadim <adin.vadim@gmail.com>
2018-06-05 19:43:08 +0300

    fix: bug with htmlwebpackplugin in dev mode

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 module.exports = (api, opts, rootOpts) => {
+  const isProd = process.env.NODE_ENV === 'production';
+  const multiPageConfig = opts.pages;
+
   api.chainWebpack(config => {
-    config
-      .plugin('html')
-      .tap(args => {
-        return [{ filename: api.resolve('templates/client/index.html') }]
-      });
+    if(isProd) {
+      if(!multiPageConfig) {
+        config
+          .plugin('html')
+          .tap(args => {
+            args[0].filename = api.resolve('templates/client/index.html');
+            return args;
+          });
+      } else {
+        const pages = Object.keys(multiPageConfig);
+        pages.forEach(name => {
+          webpackConfig
+            .plugin(`html-${name}`)
+            .tap(args => {
+              args[0].filename = api.resolve(`templates/client/${name}.html`);
+              return args;
+            });
+        })
+      }
+    }
   })
 }
